bdd3b65d36bae45561ae331be0df49c175c83f4a
Martin Sedláček <martinsedlacek7@gmail.com>
2021-03-31 10:24:01 +0200

    set linux driver

--- InstallChrome.sh
+++ InstallChrome.sh
@@ -2,5 +2,3 @@
 set -ex
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
-wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
